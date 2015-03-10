e228defecc4b600461fcaeac0a775fe8cabc2fae
Brian Bergstrom <brian.bergstrom@sportngin.com>
2015-03-10 16:34:01 -0500

    Benchmark c4.8xl for 10M with small response body.

--- test/integration/default/serverspec/perf_spec.rb
+++ test/integration/default/serverspec/perf_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 
 def siege(concurrency)
-    puts `siege -b -c #{concurrency} -t 10M localhost`
-    puts `\nvmstat -w`
-    puts `\nmpstat`
-    puts `\nnumastat`
-    puts `\ncat /home/ec2-user/siege.log`
+  describe command("siege -b -c #{concurrency} -t 10M localhost") do
+    its(:exit_status) do
+      should eq 0
+      puts `\nvmstat -w`
+      puts `\nmpstat`
+      puts `\nnumastat`
+      puts `\ncat /home/ec2-user/siege.log`
+    end
+  end
 end
 
 describe 'perf' do
@@ -17,7 +21,7 @@ describe 'perf' do
     it { should be_listening }
   end
 
-  [32].each do |concurrency|
+  [72].each do |concurrency|
     siege(concurrency)
   end
 end
